3a4bac155d9e47d61763a3590800a9116f70b0e7
Bogdan Lozev <lozev.bogdan@abv.bg>
2024-08-06 19:26:15 +0300

    Initial comit

---
+++ src/main/java/com/demo/redis/RedisDemoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.demo.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisDemoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisDemoCrudApplication.class, args);
+	}
+
+}
